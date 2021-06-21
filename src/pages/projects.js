168366a497869da5c3e665011501117b37e49e7b
Johan Melin <soldierheart@hotmail.com>
2021-06-21 14:02:50 +0200

    netlify query bug test

---
+++ src/pages/projects.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import styled from 'styled-components'
+import { Layout, Projects, Algolia } from '../components'
+
+const ProjectsPage = () => {
+  return (
+    <Wrapper>
+      <Layout>
+        <Algolia />
+      </Layout>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.main`
+  min-height: 100vh;
+  background: var(--clr-grey-10);
+  nav {
+    background: var(--clr-primary-7);
+  }
+`
+
+export default ProjectsPage
